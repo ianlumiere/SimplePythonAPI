6039b6ac3fb44425e1fa0fbc91da4fd77a4ff958
Ian Huang-Lumiere <lumie@Ians-MacBook-Air.local>
2021-01-15 17:35:02 -0600

    changing to restful functionality

--- test.py
+++ test.py
@@ -2,5 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE)
+response = requests.get(BASE+"hello/ian")
 print(response.json())
+
+# response = requests.post(BASE+"hello/ian")
+# print(response.json())
--- youtube_app.py
+++ youtube_app.py
@@ -1,15 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
+videos = {}
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"Message": "Hello World"}
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+    
+    def put(self, video_id):
+        pass
+    
 
-api.add_resource(HelloWorld, "/")
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)

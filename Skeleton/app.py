0c9180e80a5b7e39445d9459d5da7b1e05862053
Ian Huang-Lumiere <lumie@Ians-MacBook-Air.local>
2021-01-18 17:25:33 -0600

    improving basic functionality

--- Skeleton/app.py
+++ Skeleton/app.py
@@ -4,17 +4,13 @@ app = Flask(__name__)
 
 mock_list_db = [
     {
-        "pokemon_id": "0",
         "name": "Mew",
         "level": 5,
-        "description": "A special, mythical creature.",
         "in_party": True
     },
     {
-        "pokemon_id": "1",
         "name": "Pikachu",
         "level": 32,
-        "description": "A true rascal in nature.",
         "in_party": True
     }
 ]
@@ -22,17 +18,13 @@ mock_list_db = [
 # http://127.0.0.1:5000/
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    # curl -H "Content-Type: application/json" -X POST -d '{"name": "Poliwhirl", "level": 28, "in_party": false}' http://127.0.0.1:5000/
     if request.method == "POST":
         json_received = request.get_json()
+        mock_list_db.append(json_received) # add it to the dictionary
         return jsonify({"JSON Sent": json_received}), 201
     else:
         return "Welcome to the API!", 200
-
-
-# http://127.0.0.1:5000/multiply/5
-@app.route("/multiply/<int:value>", methods=['GET'])
-def multiply_10(value):
-    return jsonify({"Result": value*10}), 200
 
 
 # http://127.0.0.1:5000/pokemon_list
@@ -42,9 +34,27 @@ def get_pokemon_list():
 
 
 # http://127.0.0.1:5000/pokemon_list/0
-@app.route("/pokemon_list/<int:index>", methods=['GET'])
+@app.route("/pokemon_list/<int:index>", methods=['GET', 'PUT', 'DELETE'])
 def get_pokemon_by_index(index):
-    return jsonify({"Pokemon": mock_list_db[index]}), 200
+    # curl -H "Content-Type: application/json" -X PUT -d '{"name": "Mew", "level": 6, "in_party": false}' http://127.0.0.1:5000/pokemon_list/0
+    if request.method == "PUT":
+        json_received = request.get_json()
+        for key in json_received:
+            mock_list_db[index][key] = json_received[key]
+        return jsonify({"Updated results": mock_list_db[index]}), 200
+    # curl -X DELETE http://127.0.0.1:5000/pokemon_list/0
+    if request.method == "DELETE":
+        item_deleted = mock_list_db[index]["name"]
+        del mock_list_db[index]
+        return jsonify({"Deleted": item_deleted}), 204
+    else:
+        return jsonify({"Pokemon": mock_list_db[index]}), 200
+
+
+# http://127.0.0.1:5000/multiply/5
+@app.route("/multiply/<int:value>", methods=['GET'])
+def multiply_10(value):
+    return jsonify({"Result": value*10}), 200
 
 
 # http://127.0.0.1:5000/author?name=Ian
